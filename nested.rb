b474a97a4162284f08cf66f26063a3aa50b16e1a
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 13:20:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,11 +60,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# add the following information to the top level of programmer_hash[yukihiro_matsumoto] = {
+ :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
 
 	programmer_hash = 
  		{
